e0bb003ce2289a0d0d18dfb9bc3c56b3abcf3905
DaniyalIftikhar123 <daniyal.iftikhar108@yahoo.com>
2023-08-09 20:02:42 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,286 @@
+//********** Chapter 1 Qno1*****************
+
+//alert("Welcome to the JavaScript World!")
+
+//********** Chapter 1 Qno2*****************
+
+//alert("Error! Please Enter a Valid Password")
+
+//********** Chapter 1 Qno3*****************
+
+//alert("Welcome to JS Land...\nHappy Coding")
+
+//********** Chapter 1 Qno4*****************
+
+//alert("Welcome to JS Land")
+//alert("Happy Coding")
+
+//********** Chapter 1 Qno5*****************
+
+//alert("Hello... I can run JS through my Web Broser console")
+
+//********** Chapter 1 Qno6*****************
+
+//alert("This is my Question no 6")
+
+//********** Chapter 1 Qno7*****************
+
+//alert("I have define script in section of Head, Inside body an outside the body")
+
+//********** Chapter 2 Qno1*****************
+
+//var username;
+
+//********** Chapter 2 Qno2*****************
+
+//var myName = "Daniyal Iftikhar";
+
+//********** Chapter 2 Qno3*****************
+
+//var message = "Hello World";
+
+//alert(message)
+
+//********** Chapter 2 Qno4*****************
+
+//var std_Name = "Daniyal Iftikhar"
+//var std_Age = "24 years old"
+//var std_Certification= "Certified Mobile Application Development"
+
+//alert(std_Name)
+//alert(std_Age)
+//alert(std_Certification)
+
+//********** Chapter 2 Qno5*****************
+
+//alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+//********** Chapter 2 Qno6*****************
+
+//var email="example@example.com"
+//alert("My email address is "+ email)
+
+
+//********** Chapter 2 Qno7*****************
+
+ //var book = "A smarter Way to Learn Javascript"
+ //alert("I am trying to learn from the book "+ book)
+
+ 
+//********** Chapter 2 Qno8*****************
+
+//document.write("Yah! I can Write HTML content through Javascript");
+ 
+
+//********** Chapter 2 Qno9*****************
+
+//var JS_alert = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//alert(JS_alert)
+
+//********** Chapter 3 Qno1*****************
+
+//var age = 24;
+//var old = "years old"
+//alert("I am " + age+ " " + old)
+
+//********** Chapter 3 Qno2*****************
+
+//var visit = 14;
+
+//alert("You have visit this site "+ visit + " times")
+
+//********** Chapter 3 Qno3*****************
+
+//var birth_year = 1998;
+
+//document.write("My Birth year is" + birth_year +"<br >Data type of my declared variable is number")
+
+
+//********** Chapter 3 Qno4*****************
+
+//var visitorName = "Dani"
+//var productTitle = "T-Shirts"
+//var quantityorder = 5;
+//document.write(visitorName +" ordered "+ quantityorder+ " "+ productTitle + " on XYZ Clothing store")
+
+
+//********** Chapter 4 Qno1*****************
+
+//var userName,firstName,lastName
+
+
+//********** Chapter 4 Qno2*****************
+
+            //Legal variable name
+
+        //var name;
+        //var _name;
+        //var $name;
+        //var name1;
+       // var my_name;
+
+          //Illegal Variable Name
+
+    //var -my_name;
+    //var 1name;
+    //var my-name;
+    //var &name;
+    //var @name;
+
+
+    //********** Chapter 4 Qno3*****************
+
+    //document.write("<h1>Rules for naming JS Variable</h1>")
+    //document.writeln("<br />")
+    //document.writeln("<br /> ")
+    //document.writeln("<br /> ")
+    //document.writeln("Variable names can only contain numbers, $ and _. For example $my_1stVariable <br />")
+    //document.writeln("Variables must begin with a letter, $ or _.  For example $name, _name or name <br />")
+    //document.writeln("Variable names are case sensitive <br />")
+    //document.writeln("Variable names should not be JS keywords")
+
+
+    //********** Chapter 5 Qno1*****************
+
+    // var num1 = 3;
+    // var num2 = 5;
+    // var add= num1+num2
+    // //document.write("sum of "+ num1 + " and " + num2 + " is " +add)
+    
+    //********** Chapter 5 Qno2*****************
+
+    // var sub= num1-num2;
+    // var mul= num1*num2;
+    // var div= num1/num2;
+    // var mod= num1%num2;
+    
+    // document.write("<br /> subtraction of "+ num1 + " and " + num2 + " is " +sub)
+    // document.write("<br /> mul of "+ num1 + " and " + num2 + " is " +mul)
+    // document.write("<br /> div of "+ num1 + " and " + num2 + " is " +div)
+    //document.write("<br /> Mod of "+ num1 + " and " + num2 + " is " +mod)
+
+    
+    //********** Chapter 5 Qno3*****************
+
+    // var num;
+    // document.write("Value after variable declaration is: " + num)
+    // num=5;
+    // document.write("<br />Initial Value is "+num)
+    // num = ++num;
+    // document.write("<br />Value after increment is: " +num)
+
+    // num= num+7;
+    // document.write("<br />Value after addition is: " +num)
+    // num= --num;
+    // document.write("<br />Value after subtraction is: " +num)
+
+    // num= num%3
+    // document.write("<br />The remainder is: " +num)
+
+
+    //********** Chapter 5 Qno4*****************
+
+    // var movieTicket= 600;
+    // var TotalTicket=5;
+    // var multi=movieTicket*TotalTicket
+    // document.write("Total cost to buy 5 tickets to the movie is "+ multi+"PKR")
+
+
+    // document.write("Table of 4 <br /> <br /> <br />")
+    // document.write("4 * 1 = "+ 4*1+ "<br />")
+    // document.write("4 * 2 = "+ 4*2 + "<br />")
+    // document.write("4 * 3 = "+ 4*3+ "<br />")
+    // document.write("4 * 4 = "+ 4*4+ "<br />")
+    // document.write("4 * 5 = "+ 4*5+ "<br />")
+    // document.write("4 * 6 = "+ 4*6+ "<br />")
+    // document.write("4 * 7 = "+ 4*7+ "<br />")
+    // document.write("4 * 8 = "+ 4*8+ "<br />")
+    // document.write("4 * 9 = "+ 4*9+ "<br />")
+    // document.write("4 * 10 = "+ 4*10+ "<br />")
+    
+
+    //********** Chapter 5 Qno5*****************
+
+    // var fah= 25;
+    // var con_cel= (fah*9/5)+32;
+
+    // var cel=70;
+    // var fahren=(cel-32)*5/9
+    // document.write(fah+"<sup>0</sup> F is "+ con_cel +" <sup>0</sup> C <br />")
+    // document.write(cel+"<sup>0</sup> C is "+ fahren +" <sup>0</sup> F")
+
+    //********** Chapter 5 Qno6*****************
+
+    // var item1Price=650;
+    // var quanItem1=3;
+    // var item2Price= 100;
+    // var quanItem2 = 7;
+    // var shipCharges = 100;
+    // var item1Calc= item1Price*quanItem1;
+    // var item2Calc = item2Price*quanItem2;
+    // var totalCost = item1Calc+item2Calc+shipCharges;
+
+    // document.write("Price of Item1 is "+ item1Price + "<br />")
+    // document.write("Quantity of Item1 is  "+ quanItem1 + "<br />")
+    // document.write("Price of Item2 is "+ item2Price + "<br />")
+    // document.write("Quantity of Item2 is "+ quanItem2 + "<br />")
+    // document.write("Shipping charges "+ shipCharges + "<br /><br />")
+    // document.write("Total Cost of your order is "+ totalCost)
+
+
+    //********** Chapter 5 Qno7*****************
+    // var totalMarks=  980;
+    // var obt_Marks =804;
+    // var percent= obt_Marks/totalMarks*100;
+
+    // document.write("Total Marks "+ totalMarks)
+    // document.write("obtained Marks"+ obt_Marks)
+    // document.write("Percentage "+ percent+ "%")
+
+
+    //********** Chapter 5 Qno8*****************
+
+    // var Pak_curr= (10*104.80)+(25*28)
+    // document.write("Total Currency in PKR: "+ Pak_curr)
+
+    //********** Chapter 5 Qno9*****************
+
+    // var num = 6;
+    // num = num+5*10/2;
+    // document.write(num)
+
+    //********** Chapter 5 Qno10*****************
+
+    // var currYear = 2023
+    // var birthYear = 1998
+    // var age = currYear-birthYear;
+
+    // document.write("Current Year is "+ currYear+ "<br />")
+    // document.write("Birth Year is "+ birthYear + "<br />")
+    // document.write("Yor age is "+ age + "<br /> ")
+
+    //********** Chapter 5 Qno11*****************
+
+    // var radius=20;
+    // var pi=3.142
+    // var circum= 2*pi*radius;
+    // var area = pi*radius*radius;
+    // document.write("Radius of a circle is "+ radius+"<br />")
+    // document.write("The circumference is "+ circum+"<br />")
+    // document.write("The area is "+area)
+
+    //********** Chapter 5 Qno11*****************
+
+    // var snack = "burger"
+    // var age=15
+    // var max_age=65;
+    // var perDay = 3;
+    // var totalSnack=(perDay*365)*(max_age-age)
+    // document.write("Your favourite snack "+ snack+ "<br />")
+    // document.write("Your age "+age + "<br />")
+    // document.write("Maximum Age "+ max_age + ",br />")
+    // document.write("Amount of snacks per day "+ perDay+"<br />")
+    
+    // document.write("You will need "+ totalSnack + " to last you until the ripe old age of "+max_age +".")
+
+
